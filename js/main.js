380785e4d0278f78c8f89a92afd692e65c190d7f
braynaci24 <aynacimuhammetemin@gmail.com>
2021-05-24 14:23:07 +0300

    responsive

--- js/main.js
+++ js/main.js
@@ -8,10 +8,11 @@ $(document).ready(function () {
     }
     $('.send-button').click(function () {
         let inputVal = $('.todo-info').val();
-        todoData.push(inputVal);
-        localStorage.setItem('todo', JSON.stringify(todoData));
+       
 
         if(inputVal.length >= 4){
+            todoData.push(inputVal);
+            localStorage.setItem('todo', JSON.stringify(todoData));
             $('.todo-list').append(` <div class="todo"><span class="todo-name">${inputVal}</span><i class="fas fa-trash-alt trash"></i></div>`)
             $('.todo-info').val("");
 
